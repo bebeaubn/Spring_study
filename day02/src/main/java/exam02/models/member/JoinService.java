3e3e9b5733463d27795797ab6b6ea9f6953d08c7
bebeaubn <yebin0213@naver.com>
2023-10-24 18:06:11 +0900

    .

---
+++ day02/src/main/java/exam02/models/member/JoinService.java
@@ -0,0 +1,32 @@
+package exam02.models.member;
+
+import exam02.commons.Validator;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class JoinService {
+    @Autowired
+    private Validator <Member>validator;
+    @Autowired
+    private MemberDao memberDao;
+
+    /*
+    public JoinService(Validator <Member> validator, MemberDao memberDao) {
+        this.validator = validator;
+        this.memberDao = memberDao;
+    }
+    */
+
+    public void join(Member member) {
+
+        validator.check(member);
+
+        memberDao.register(member);
+    }
+
+
+}
