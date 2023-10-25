f054249d294771a754429e471d61ca5dcce254df
bebeaubn <yebin0213@naver.com>
2023-10-25 17:51:05 +0900

    day02 수업내용 왜 안올라 가지냐 욕나오게

--- src/main/java/exam05/Message2.java
+++ src/main/java/exam05/Message2.java
@@ -2,6 +2,18 @@ package exam05;
 
 public class Message2 {
 
+    public void send(String message) {
+        System.out.printf("전송메서지 : %s%n", message);
 
+    }
+
+
+    public void init(){  //초기화전에 호출
+        System.out.println("init!!");
+    }
+    public void close() { //소멸시전에 호출
+        System.out.println("close!!");
+
+    }
 
 }
